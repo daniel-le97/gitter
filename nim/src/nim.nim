d1e1100a287a52be96fbc340562542437f121c48
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2025-04-30 21:11:24 -0600

    adjust nim example to use nim style guide indentation

--- nim/src/nim.nim
+++ nim/src/nim.nim
@@ -2,21 +2,21 @@ import os, times, strutils
 
 # Define the Repo structure
 type
-  Repo = object
-    path: string
-    url: string
+    Repo = object
+        path: string
+        url: string
 
 
 # Function to extract the GitHub repository URL from a .git/config file
 proc getGitHubRepoName(configPath: string): string =
-  if not fileExists(configPath):
-    return "not published on GitHub"
+    if not fileExists(configPath):
+        return "not published on GitHub"
 
-  let content = readFile(configPath)
-  for line in content.splitLines():
-    if line.startsWith("\turl = "): # Matches the tab character before "url ="
-      return line.split("\turl = ")[1]
-  return "not published on GitHub"
+    let content = readFile(configPath)
+    for line in content.splitLines():
+        if line.startsWith("\turl = "): # Matches the tab character before "url ="
+            return line.split("\turl = ")[1]
+    return "not published on GitHub"
 
 # Function to read a directory recursively
 proc readDirectoryRecursively(path: string): seq[Repo] =
@@ -37,21 +37,21 @@ proc readDirectoryRecursively(path: string): seq[Repo] =
 
 # Main procedure
 proc main() =
-  let startTime = cpuTime()
-  let home = getEnv("HOME")
-  let ok = home & "/homelab"
-  let skipDirs = @[home & "/.", home & "/Library"]
-  echo "Searching for .git/config files in ", ok
-  # findGitRepos(home, skipDirs)
+    let startTime = cpuTime()
+    let home = getEnv("HOME")
+    let ok = home & "/homelab"
+    let skipDirs = @[home & "/.", home & "/Library"]
+    echo "Searching for .git/config files in ", ok
+    # findGitRepos(home, skipDirs)
 
-  let repos = readDirectoryRecursively(ok)
-  for index, repo in repos :
-      echo index, ". ", repo.path
-      echo " - ", repo.url
-  
-  echo "Total repositories found: ", repos.len
-  let endTime = cpuTime()
-  let elapsedTime = endTime - startTime
-  echo "Elapsed time: ", elapsedTime, " seconds"
+    let repos = readDirectoryRecursively(ok)
+    for index, repo in repos :
+        echo index, ". ", repo.path
+        echo " - ", repo.url
+
+    echo "Total repositories found: ", repos.len
+    let endTime = cpuTime()
+    let elapsedTime = endTime - startTime
+    echo "Elapsed time: ", elapsedTime, " seconds"
 # Run the main procedure
 main()
